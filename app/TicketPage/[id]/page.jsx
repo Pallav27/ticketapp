9d4c63fe9aa3a5479b08f03107ca10489103f3c3
Pallav27 <pallavshrm27@gmail.com>
2025-02-16 14:34:28 +0530

    ticektingapp

---
+++ app/TicketPage/[id]/page.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const TicketPage = ({params}) => {
+  return (
+    <div className='bg-red-400'>TicketPage {params.id}</div>
+  )
+}
+
+export default TicketPage
